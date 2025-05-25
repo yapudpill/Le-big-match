8420556cf0adacd0ebc4116f8b607f4d78955737
Anthony Fernandes <anthony.fernandes@etu.u-paris.fr>
2025-05-25 23:33:17 +0200

    remplissage prefs et tags

--- matching.sql
+++ matching.sql
@@ -1,7 +1,7 @@
 \set util 'fernanda'
 
 drop function if exists score_ecart;
-create function score_ecart (val int, seuil int) returns int as $$
+create function score_ecart (val numeric, seuil numeric) returns numeric as $$
   select greatest(0, 1 - (val / seuil)^2) as score
 $$ language SQL;
 
@@ -20,7 +20,7 @@ with compatibilite as (
          T2 as (select tag from tag_utilisateur where utilisateur = u.id),
          I as ((select * from T1) intersect all (select * from T2)),
          U as ((select * from T1) union all (select * from T2))
-    select (select count(*) from I) / greatest((select count(*) from U), 1) as score_t
+    select 1. * (select count(*) from I) / greatest((select count(*) from U), 1.) as score_t
   )
   from compatibilite u
 ), score_evenements as (
@@ -31,10 +31,11 @@ with compatibilite as (
          T2 as (select evenement from presence where utilisateur = u.id),
          I as ((select * from T1) intersect all (select * from T2)),
          U as ((select * from T1) union all (select * from T2))
-  select (select count(*) from I) / greatest((select count(*) from U), 1) as score_e
+  select 1. * (select count(*) from I) / greatest((select count(*) from U), 1.) as score_e
   )
   from compatibilite u
 ), score_preferences as (
+  -- Score de proximité entre l'utilisateur a et les critères de util
   select a.id, max(
     0.5 * (extract(year from age(a.naissance)) between coalesce(age_min,0) and coalesce(age_max,100))::int
     + coalesce(0.125 * score_ecart(a.taille - p.taille, 10), 0)
@@ -49,6 +50,7 @@ with compatibilite as (
      where chercheur = :'util') p
   group by a.id
 )
+-- On assemble tous les scores
 select id, (0.5 * score_p + 0.3 * score_e + 0.2 * score_t) as score
 from
   score_tags
--- remplissage.sql
+++ remplissage.sql
@@ -1,119 +1,132 @@
 -- GÉOGRAPHIE (données de l'INSEE) --
 create temporary table tmp_reg (
-    reg char(2),
-    cheflieu char(5),
-    tncc char(1),
-    ncc text,
-    nccenr text,
-    libelle text
+  reg char(2),
+  cheflieu char(5),
+  tncc char(1),
+  ncc text,
+  nccenr text,
+  libelle text
 );
 
 \copy tmp_reg from 'data/region.csv' with (format csv, header true)
 insert into region (code, nom)
-    select reg, libelle from tmp_reg;
+  select reg, libelle from tmp_reg;
 
 drop table tmp_reg;
 
 create temporary table tmp_dep (
-    dep char(3),
-    reg char(2),
-    cheflieu char(5),
-    ttcn char(1),
-    ncc text,
-    nccenr text,
-    libelle text
+  dep char(3),
+  reg char(2),
+  cheflieu char(5),
+  ttcn char(1),
+  ncc text,
+  nccenr text,
+  libelle text
 );
 
 \copy tmp_dep from 'data/departement.csv' with (format csv, header true)
 insert into departement (code, nom, region)
-    select dep, libelle, reg from tmp_dep;
+  select dep, libelle, reg from tmp_dep;
 
 drop table tmp_dep;
 
 create temporary table tmp_ville (
-    typecom char(4),
-    com char(5),
-    reg char(2),
-    dep char(3),
-    ctcd char(4),
-    arr char(4),
-    tncc char(1),
-    ncc text,
-    nccenr text,
-    libelle text,
-    can char(5),
-    comparent char(5)
+  typecom char(4),
+  com char(5),
+  reg char(2),
+  dep char(3),
+  ctcd char(4),
+  arr char(4),
+  tncc char(1),
+  ncc text,
+  nccenr text,
+  libelle text,
+  can char(5),
+  comparent char(5)
 );
 
 \copy tmp_ville from 'data/commune.csv' with (format csv, header true)
 insert into ville (code, nom, departement)
-    select com, libelle, dep from tmp_ville where typecom = 'COM';
+  select com, libelle, dep from tmp_ville where typecom = 'COM';
 
 drop table tmp_ville;
 
 -- UTILISATEURS --
 create temporary table tmp_util (
-    id text,
-    nom text,
-    prenom text,
-    naissance date,
-    bio text,
-    ville char(5),
-    abonne boolean,
-    inscription date
+  id text,
+  nom text,
+  prenom text,
+  naissance date,
+  bio text,
+  ville char(5),
+  abonne boolean,
+  inscription date
 );
 
 \copy tmp_util from 'data/utilisateur.csv' with (format csv, header true)
 insert into utilisateur (id, nom, prenom, naissance, bio, ville, abonne, inscription)
-    select id, nom, prenom, naissance, bio, ville, abonne, inscription
-    from tmp_util;
+  select id, nom, prenom, naissance, bio, ville, abonne, inscription
+  from tmp_util;
 
 drop table tmp_util;
 
 create temporary table tmp_desc (
-    id text,
-    genre genre_t,
-    pref pref_t,
-    couleur_cheveux text,
-    couleur_yeux text,
-    poids int,
-    taille int
+  id text,
+  genre genre_t,
+  pref pref_t,
+  couleur_cheveux text,
+  couleur_yeux text,
+  poids int,
+  taille int
 );
 
 \copy tmp_desc from 'data/description.csv' with (format csv, header true)
 insert into description (id, genre, pref, couleur_cheveux, couleur_yeux, poids, taille)
-    select id, genre, pref, couleur_cheveux, couleur_yeux, poids, taille
-    from tmp_desc;
+  select id, genre, pref, couleur_cheveux, couleur_yeux, poids, taille
+  from tmp_desc;
 
 drop table tmp_desc;
 
 create temporary table tmp_avis (
-    source text,
-    destination text,
-    type_avis avis_t,
-    date_avis date
+  source text,
+  destination text,
+  type_avis avis_t,
+  date_avis date
 );
 
 \copy tmp_avis from 'data/avis.csv' with (format csv, header true)
 insert into avis (source, destination, type_avis, date_avis)
-    select source, destination, type_avis, date_avis
-    from tmp_avis;
+  select source, destination, type_avis, date_avis
+  from tmp_avis;
 
 drop table tmp_avis;
 
 create temporary table tmp_preference (
-    chercheur text,
-    couleur_cheveux text,
-    couleur_yeux text,
-    poids int,
-    taille int,
-    age_min int,
-    age_max int
+  chercheur text,
+  couleur_cheveux text,
+  couleur_yeux text,
+  poids int,
+  taille int,
+  age_min int,
+  age_max int
 );
 
 \copy tmp_preference from 'data/preference.csv' with (format csv, header true)
 insert into preference (chercheur, couleur_cheveux, couleur_yeux, poids, taille, age_min, age_max)
-    select chercheur, couleur_cheveux, couleur_yeux, poids, taille, age_min, age_max
-    from tmp_preference;
+  select chercheur, couleur_cheveux, couleur_yeux, poids, taille, age_min, age_max
+  from tmp_preference;
 
 drop table tmp_preference;
+
+create temporary table tmp_tag_u (
+  utilisateur text,
+  tag text,
+  source text
+);
+
+\copy tmp_tag_u from 'data/tag_utilisateur.csv' with (format csv, header true)
+insert into tag_utilisateur (utilisateur, tag, source)
+  select distinct utilisateur, tag, source
+  from tmp_tag_u;
+
+drop table tmp_tag_u;
