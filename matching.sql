748188fcb7c42e60320fd23ac3ef2f1a366e54cc
Marc <suga75013@gmail.com>
2025-05-25 12:15:00 +0200

    requête matching

--- matching.sql
+++ matching.sql
@@ -6,7 +6,7 @@ create function score_ecart (val int, seuil int) returns int as $$
 $$ language SQL;
 
 -- Compatibilité
-select u2.id, u2.nom, u2.prenom
+select u2.id, u2.nom, u2.prenom, u2.naissance
 from util_desc u1, util_desc u2
 where
   u1.id = :'util' and u1.id <> u2.id
@@ -25,7 +25,12 @@ select u.id, (
     greatest((select count(*) from ((select *  from T1) union (select * from T2)) t), 1)
     as score
 )
-from utilisateur u;
+from (select u2.id, u2.nom, u2.prenom
+from util_desc u1, util_desc u2
+where
+  u1.id = :'util' and u1.id <> u2.id
+  and (u1.pref is null or u1.pref::text in ('tout', u2.genre::text))
+  and (u2.pref is null or u2.pref::text in ('tout', u1.genre::text))) u;
 
 
 -- Scores de Critère
@@ -36,18 +41,18 @@ from utilisateur u;
 --score taille -> score_ecart (notre_taille - taille_recommandation,20)
 --score poids -> score_ecart (notre_poids - poids_recommandation,20)
 
- with compatibilite as (
-  select *
+with compatibilite as (
+  select u2.id, u2.nom, u2.prenom, u2.naissance
   from util_desc u1, util_desc u2
   where
-  u1.id = :'util' and u1.id <> u2.id
+    u1.id = :'util' and u1.id <> u2.id
     and (u1.pref is null or u1.pref::text in ('tout', u2.genre::text))
     and (u2.pref is null or u2.pref::text in ('tout', u1.genre::text))
 )
 select
     a.id,
     max(
-        coalesce(0.5   * (extract(year from age(naissance))
+        coalesce(0.5   * (extract(year from age(u.naissance))
           between coalesce(age_min,0) and coalesce(age_max,100))::int, 0) +
         coalesce(0.125 * score_ecart((a.taille - p.taille), 10), 0) +
         coalesce(0.125 * score_ecart((a.poids - p.poids), 10), 0) +
@@ -73,7 +78,71 @@ select u.id, (
     greatest((select count(*) from ((select *  from E1) union (select * from E2)) t), 1)
     as score
 )
-from utilisateur u;
+from (select u2.id, u2.nom, u2.prenom
+from util_desc u1, util_desc u2
+where
+  u1.id = :'util' and u1.id <> u2.id
+  and (u1.pref is null or u1.pref::text in ('tout', u2.genre::text))
+  and (u2.pref is null or u2.pref::text in ('tout', u1.genre::text))) u;
+
+
+-- Requête avec score final
+
+with compatibilite as (
+  select u2.id, u2.nom, u2.prenom, u2.naissance
+  from util_desc u1, util_desc u2
+  where
+    u1.id = :'util' and u1.id <> u2.id
+    and (u1.pref is null or u1.pref::text in ('tout', u2.genre::text))
+    and (u2.pref is null or u2.pref::text in ('tout', u1.genre::text))
+), score_tags as (
+    select u.id, (
+    with T1 as (
+      select tag from tag_utilisateur where utilisateur = :'util'
+    ), T2 as (
+      select tag from tag_utilisateur where utilisateur = u.id
+    )
+    select
+      (select count(*) from ((select *  from T1) intersect (select * from T2)) t) /
+      greatest((select count(*) from ((select *  from T1) union (select * from T2)) t), 1)
+      as score_t
+    )
+    from utilisateur u
+  ), score_evenements as (
+    select u.id, (
+    with E1 as (
+      select evenement from presence p where p.utilisateur = :'util'
+    ), E2 as (
+      select evenement from presence p where p.utilisateur = u.id
+    )
+    select
+      (select count(*) from ((select *  from E1) intersect (select * from E2)) t) /
+      greatest((select count(*) from ((select *  from E1) union (select * from E2)) t), 1)
+      as score_e
+    )
+    from utilisateur u
+  ), score_preferences as (
+    select a.id,
+        max(
+            coalesce(0.5   * (extract(year from age(u.naissance))
+              between coalesce(age_min,0) and coalesce(age_max,100))::int, 0) +
+            coalesce(0.125 * score_ecart((a.taille - p.taille), 10), 0) +
+            coalesce(0.125 * score_ecart((a.poids - p.poids), 10), 0) +
+            coalesce(0.125 * (a.couleur_cheveux = p.couleur_cheveux)::int, 0) +
+            coalesce(0.125 * (a.couleur_yeux = p.couleur_yeux)::int, 0)
+        ) as score_p
+    from  (select * from description where id <> :'util') a,
+          compatibilite u,
+          (select * from preference where chercheur = :'util') p
+    where u.id = a.id
+    group by a.id
+  )
+select sp.id, (0.5*score_p + 0.3*score_e + 0.2*score_t) as score
+from score_preferences sp, score_evenements se, score_tags st
+where sp.id = se.id and se.id = st.id;
 
 
 \unset util
+
+
+
